bd48a073ca5bb93b17900fa483f27cdf16ccc80c
Arnold Adamson <arnold.ametlik@gmail.com>
2018-05-21 19:48:08 +0300

    Initial commit

---
+++ src/BeerSong.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+
+public class BeerSong {
+    public static void main(String[] args) {
+        int beerCount = 50;
+        String word = " õlut ";
+
+        while (beerCount > 0) {
+            System.out.println("Riiulil on " + beerCount + word);
+            beerCount--;
+            System.out.println("Võtad ühe ära ja alles jääb " + beerCount);
+
+            if (beerCount == 1) {
+                word = " Õlu";
+
+            }
+
+            if (beerCount == 0) {
+                System.out.println("Õlu on otsat :(");
+            }
+
+
+        }
+
+    }
+}
