daeaa3aa97d10ed3cc7355ffec7589a8d829091d
Arnold Adamson <arnold.ametlik@gmail.com>
2018-05-21 21:53:00 +0300

    ignore

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -16,7 +16,7 @@ public class BeerSong {
             }
 
             if (beerCount == 0) {
-                System.out.println("Õlu on otsat :(");
+                System.out.println("Õlu on kahjuks otsaadwadat :(");
             }
 
 
